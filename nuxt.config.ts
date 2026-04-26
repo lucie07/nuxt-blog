f0d9c115cae8cc9c795327a96d4e1cdc1242fc6c
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-04-26 20:07:18 +0100

    Change to light mode in default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,8 +74,8 @@ export default defineNuxtConfig({
 
   colorMode: {
     classSuffix: '',
-    preference: 'dark',
-    fallback: 'light',
+    preference: 'light',
+    fallback: 'dark',
   },
 
   content: {
