cb4d4434d44b6fe699d23b60051fbb793f8b336b
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-04-27 02:12:27 +0100

    Update blog listing

--- app/pages/blogs/index.vue
+++ app/pages/blogs/index.vue
@@ -2,7 +2,11 @@
 import Fuse from 'fuse.js'
 import type { BlogPost } from '~/types/blog'
 
-const { data } = await useAsyncData('all-blog-post', () => queryCollection('content').all())
+const { data } = await useAsyncData('all-blog-post', () =>
+  queryCollection('content')
+    .where('path', 'LIKE', '/blogs/%')
+    .all()
+)
 
 const elementPerPage = ref(10)
 const pageNumber = ref(1)
