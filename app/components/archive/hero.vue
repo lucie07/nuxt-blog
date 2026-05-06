54d9f946fc6ec84cac3fb06f3f95c53384186b0c
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-06 23:15:23 +0100

    Remove dog paw image on all blogs page

--- app/components/archive/hero.vue
+++ app/components/archive/hero.vue
@@ -3,19 +3,17 @@ import { blogsPage } from '~/data'
 </script>
 
 <template>
-  <div class="container mx-auto mb-5">
-    <div class="grid grid-cols-1 sm:grid-cols-2 items-center">
-      <div class="px-6">
-        <h1 class="text-black dark:text-zinc-300 font-semibold leading-tight text-4xl md:text-5xl my-5">
+  <section class="container mx-auto mb-6">
+    <div class="px-6 py-8 md:py-12">
+      <div class="max-w-4xl">
+        <h1 class="text-black dark:text-zinc-300 font-semibold leading-tight text-4xl md:text-5xl mb-5">
           {{ blogsPage.title }}
         </h1>
-        <p class="dark:text-zinc-300">
+
+        <p class="max-w-3xl text-zinc-600 dark:text-zinc-300 text-base md:text-lg leading-relaxed">
           {{ blogsPage.description }}
         </p>
       </div>
-      <div class="px-6 justify-self-center">
-        <LogoDogpow />
-      </div>
     </div>
-  </div>
+  </section>
 </template>
