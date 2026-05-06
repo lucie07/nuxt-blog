d747f618fb499738b29c2228960c06d6025d7777
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-06 21:35:07 +0100

    Remove top footer section

--- app/components/main/footer.vue
+++ app/components/main/footer.vue
@@ -7,13 +7,6 @@ const path = computed(() => route.fullPath.replace('/', ''))
 <template>
   <div class="py-5 border-t dark:border-gray-800 mt-5 text-zinc-700 dark:text-zinc-300">
     <div class="px-6 container max-w-5xl mx-auto">
-      <div class="grid grid-cols-1 md:grid-cols-3">
-        <FooterSite v-if="path === 'about'" />
-        <FooterDeveloper v-else />
-        <FooterLink />
-        <FooterConnect />
-      </div>
-
       <div class="border-t dark:border-gray-800 mt-5 text-center p-2">
         © 2026 Lucie Mingmei Hao
       </div>
